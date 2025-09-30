1502862ec78af014c06a74f003e8836267006ef7
neclor <neclordev@gmail.com>
2025-09-30 20:34:02 +0200

    4.0.0,5

--- BallisticSolutions/VectorExtensions/Vector2Extensions.cs
+++ BallisticSolutions/VectorExtensions/Vector2Extensions.cs
@@ -8,15 +8,12 @@ using Vector3 = System.Numerics.Vector3;
 using Vector4 = System.Numerics.Vector4;
 #endif
 
-
 namespace BallisticSolutions.VectorExtensions;
 
-
-[System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static")]
 internal static class Vector2Extensions {
 
 #if GODOT
-	private static readonly Vector2 _nan = new Vector2(float.NaN, float.NaN);
+	private static readonly Vector2 _nan = new(float.NaN, float.NaN);
 #endif
 
 	extension(Vector2 v) {
@@ -25,9 +22,9 @@ internal static class Vector2Extensions {
 		public static Vector2 NaN => _nan;
 #endif
 
-		public Vector3 ToVector3(float z = 0f) => new Vector3(v.X, v.Y, z);
+		public Vector3 ToVector3(float z = 0f) => new(v.X, v.Y, z);
 
-		public Vector4 ToVector4(float z = 0f, float w = 0f) => new Vector4(v.X, v.Y, z, w);
+		public Vector4 ToVector4(float z = 0f, float w = 0f) => new(v.X, v.Y, z, w);
 
 		public static Vector2 From(Vector3 from) => from.ToVector2();
 
