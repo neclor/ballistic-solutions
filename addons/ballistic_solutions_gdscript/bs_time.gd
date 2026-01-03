aeed463e7651704b9d3410a15b73816c03090a06
neclor <neclordev@gmail.com>
2026-01-03 23:37:44 +0100

    Update bs_time.gd

--- addons/ballistic_solutions_gdscript/bs_time.gd
+++ addons/ballistic_solutions_gdscript/bs_time.gd
@@ -8,7 +8,7 @@
 
 ## Computes all possible interception times between a projectile and a moving target. [br]
 ## [b]Returns:[/b] A sorted array of all valid interception times (t > 0). Empty if interception is impossible.
-static func all_impact_times_by_direction(projectile_direction: float, to_target: Variant, target_velocity: Variant = Vector4.ZERO, projectile_acceleration: Variant = Vector4.ZERO, target_acceleration: Variant = Vector4.ZERO) -> Array[float]:
+static func all_impact_times(projectile_speed: float, to_target: Variant, target_velocity: Variant = Vector4.ZERO, projectile_acceleration: Variant = Vector4.ZERO, target_acceleration: Variant = Vector4.ZERO) -> Array[float]:
 	var type: Variant.Type = typeof(to_target)
 	match type:
 		Variant.Type.TYPE_VECTOR2, Variant.Type.TYPE_VECTOR2I: return all_impact_times_by_speed_vector2(projectile_speed, to_target, BsVector2Extensions.from_vector(target_velocity), BsVector2Extensions.from_vector(projectile_acceleration), BsVector2Extensions.from_vector(target_acceleration))
@@ -56,14 +56,10 @@ static func all_impact_times_by_speed_vector4(projectile_speed: float, to_target
 	var d: float = 2 * target_velocity.dot(to_target)
 	var e: float = to_target.length_squared()
 
-	var all_impact_times: Array[float] = Array(
-		Res.quartic(a, b, c, d, e).filter(func(i: float) -> bool: return i > 0),
-		TYPE_FLOAT,
-		"",
-		null
-	)
+	var all_impact_times: Array[float] = Array(Res.quartic(a, b, c, d, e).filter(func(i: float) -> bool: return i > 0), TYPE_FLOAT, "", null)
 	all_impact_times.sort()
 	return all_impact_times
+
 
 
 
@@ -74,13 +70,11 @@ static func all_impact_times_by_direction_vector4(projectile_direction: Vector4,
 
 	
 
-
-
-	var a: float = relative_acceleration.length_squared() / 4
-	var b: float = -relative_acceleration.dot(target_velocity)
-	var c: float = target_velocity.length_squared() - relative_acceleration.dot(to_target) - projectile_speed * projectile_speed
-	var d: float = 2 * target_velocity.dot(to_target)
-	var e: float = to_target.length_squared()
+	var a: float = 0
+	var b: float = 0
+	var c: float = 0
+	var d: float = 0
+	var e: float = 0
 
 	var all_impact_times: Array[float] = Array(
 		Res.quartic(a, b, c, d, e).filter(func(i: float) -> bool: return i > 0),
@@ -124,5 +118,5 @@ static func best_impact_time_by_speed_vector2(projectile_speed: float, to_target
 ## See [method best_impact_time].
 static func best_impact_time_by_speed_vector4(projectile_speed: float, to_target: Vector4, target_velocity: Vector4 = Vector4.ZERO, projectile_acceleration: Vector4 = Vector4.ZERO, target_acceleration: Vector4 = Vector4.ZERO) -> float:
 	if projectile_speed < 0: _BsLogger._push_warning("`BsTime.best_impact_time`: Negative `projectile_speed`.")
-	var impact_times: Array[float] = all_impact_times_vector4(projectile_speed, to_target, target_velocity, projectile_acceleration, target_acceleration);
+	var impact_times: Array[float] = all_impact_times_by_speed_vector4(projectile_speed, to_target, target_velocity, projectile_acceleration, target_acceleration);
 	return NAN if impact_times.is_empty() else impact_times[0];
